9bbef9f718b8a3c09c857c9779a7be4026022aee
damonliftz <aidendamon16@gmail.com>
2025-11-03 12:10:40 +0200

    functions.js

---
+++ functions.js
@@ -0,0 +1,11 @@
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+function add(a, b) {
+  return a + b;
+}
+const greeting = greet("Aiden");
+const sum = add(7, 3);
+
+console.log(greeting); 
+console.log("Sum:", sum); 
